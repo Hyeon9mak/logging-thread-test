b35e713bc924ba528031952c7f239f0a530722cc
Hyeon9mak <jinha3507@gmail.com>
2021-11-14 19:44:17 +0900

    feat: 프로젝트 초기 셋팅

---
+++ src/main/java/wooteco/thread/logging/ThreadPool/ThreadPoolApplication.java
@@ -0,0 +1,12 @@
+package wooteco.thread.logging.ThreadPool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreadPoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThreadPoolApplication.class, args);
+	}
+}
---
+++ src/test/java/wooteco/thread/logging/ThreadPool/ThreadPoolApplicationTests.java
@@ -0,0 +1,8 @@
+package wooteco.thread.logging.ThreadPool;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThreadPoolApplicationTests {
+
+}
